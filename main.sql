49430335fd44cf2ca70a4b3e35d6dd33f53219a0
92Chetan <chetansinghal@outlook.com>
2023-10-28 05:57:39 +0000

    Commit -(28-10-2023)

--- main.sql
+++ main.sql
@@ -1,3 +1,22 @@
---Hanna excels in calculating binary data and is now interviewing for the position of sql developer. The interviewer ask her how much storage DATETIME is needed ?
+--------- SQL Advanced Naukri.com BADGE QUESTION & ANSWER ---------------
+
+-- 1). Hanna excels in calculating binary data and is now interviewing for the position of sql developer. The interviewer ask her how much storage DATETIME is needed ?
 
 DATETIME datatype in SQLSERVER takes upto 8 bytes of storage (4 bytes for DATE & 4 bytes for TIME).
+
+  -- 2). Darren was a database engineer. His project includes storing data and maintains it in a system. So he needs to store Character data in the database, so how can it be stored ?
+
+  -- 3). Following are the use of well- designed indexes ?
+
+  a). Improves the query performance
+  b). Reduce storage index costs.
+  c). Reduce index maintainance cost.
+
+  -- 4). Kim went for a data scientist interview, then an interveiwer ask a simple question. What category this (COUNT) keyword belongs to ?
+
+  COUNT keyword belong to aggregate function.
+
+  
+  
+
+  
