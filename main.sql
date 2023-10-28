e24c776dd14c3c6cad919df0245b95b259ff65e5
92Chetan <chetansinghal@outlook.com>
2023-10-28 04:43:25 +0000

    Initial Commit

---
+++ main.sql
@@ -0,0 +1,3 @@
+--Hanna excels in calculating binary data and is now interviewing for the position of sql developer. The interviewer ask her how much storage DATETIME is needed ?
+
+DATETIME datatype in SQLSERVER takes upto 8 bytes of storage (4 bytes for DATE & 4 bytes for TIME).
